241cacc224201dda2da1759b2e7053d4e7a72c20
Sagiv Ofek <sagiv4@gmail.com>
2017-01-23 22:06:10 -0500

    add config file and released db to public

---
+++ models/db.js
@@ -0,0 +1,18 @@
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DATABASE_URL,
+  FIREBASE_STORAGE_BACKET,
+  FIREBASE_MESSAGING_SENDER_ID } from '../config';
+
+import firebase from 'firebase';
+
+var config = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: FIREBASE_DATABASE_URL,
+  storageBucket: FIREBASE_STORAGE_BACKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+};
+firebase.initializeApp(config);
+export default firebase.database();
